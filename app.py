b97183a39107974f855516d0f9bc3041d1f90d0f
André Luz <andreelluz7@gmail.com>
2025-10-16 15:50:48 -0300

    app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-from flask import Flask
-app = Flask(__name__)  # <- ESSA VARIÁVEL PRECISA EXISTIR
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Aplicação Flask no Azure funcionando!"
+
+if __name__ == '__main__':
+    app.run()
