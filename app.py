d598bd3568e93762574ca225ad2ecf4cfcdb58f5
André Luz <andreelluz7@gmail.com>
2025-10-16 16:03:38 -0300

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+contador = 0
 
 @app.route('/')
 def index():
-    return "Aplicação Flask no Azure funcionando!"
+    global contador
+    contador += 1
+    return render_template('index.html', contador=contador)
 
 if __name__ == '__main__':
     app.run()
